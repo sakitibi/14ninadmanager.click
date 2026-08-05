e5084602bc641a8af773e5e64381ec47549fa1be
さきちび <sakitibi10nin@icloud.com>
2026-08-05 17:36:35 +0900

    Update allowedUser.ts

--- config/allowedUser.ts
+++ config/allowedUser.ts
@@ -1,9 +1,7 @@
 export const ALLOWED_USER_IDS:string[] = [ // 管理者のuser_idリスト
     "9d2347a2-6322-4d55-93f1-71d8440e5f32",
-    "b23a6b6a-9ec2-4ea8-b492-371d0dd7c958",
+    "ab1ed29a-007d-454b-a400-764b4db18e86",
     "ba159f68-814f-4564-935f-cea46624fd53",
     "96b00cd8-d43b-4a5b-b473-84433c0b3c98",
-    "232e8a18-b82f-46e3-983c-4f2abfb37e7e",
-    "a82a5ff2-6918-4205-8b00-a443df1e65da",
-    "b31cd12c-5312-4a4d-aed6-0f0f98babb7e"
+    "b436ac5e-f950-48b2-9557-83fce2d2c671",
 ];
